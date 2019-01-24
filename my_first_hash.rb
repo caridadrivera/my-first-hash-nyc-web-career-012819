b56f14bc1e930e7e743cebcb790d51ccd21ce49c
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-24 21:05:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
- the_manifest = { "bones" => 5, 
+ the_manifest = { "whale bone corsets" => 5, 
                 "porcelain vases" => 2,
                 "oil paintings" => 3
                 }
@@ -38,6 +38,6 @@ def adding
 
   # return the shipping_manifest hash below
   
- return shipping_manifest
+  shipping_manifest
 
 end
